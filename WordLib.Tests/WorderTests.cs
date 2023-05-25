8bdd797e4715e8080851382bb6fac8faa4f1b529
Chrisvasa <Christophervasankari@gmail.com>
2023-05-25 07:33:28 +0200

    Created WordLib.Tests and WordLib. Test currently fails and throws a NotYetImplementedException

---
+++ WordLib.Tests/WorderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WordLib.Tests
+{
+    public class WorderTests
+    {
+        [Fact]
+        public void PalindromeShouldReturnReversedInput()
+        {
+            // Arrange
+            Worder word = new Worder();
+            string expected = "tset";
+            // Act
+            string actual = word.Palindrome();
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ WordLib/Worder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLib
+{
+    public class Worder
+    {
+        public string Palindrome()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
